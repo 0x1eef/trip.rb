c7b5ef23f9e305330f866fcfc9940c6cb8d8673b
0x1eef <0x1eef@protonmail.com>
2021-08-25 22:56:29 -0300

    update Trip::Event docs

--- lib/trip/event.rb
+++ lib/trip/event.rb
@@ -1,108 +1,83 @@
 # frozen_string_literal: true
 
 class Trip::Event < BasicObject
-  #
   # @return [String]
-  #   Returns the type of event as reported by the `Thread#set_trace_func` API
-  #   (eg "c-call", "call", "c-return", "return", ...),
-  #
+  #  Returns the type of event as reported by the "Thread#set_trace_func" API.
+  #  Examples: "c-call", "call", "c-return", "return", ...
   attr_reader :type
 
-  #
-  # @api private
-  #
   def initialize(type, event)
     @type = type
     @event = event
   end
 
-  #
   # @return [String]
-  #   Returns the path to a file where an event occurred.
-  #
+  #  Returns the path where an event occurred.
   def file
     @event[:file]
   end
 
-  #
   # @return [Integer]
-  #   Returns the line number where an event occurred,
-  #
+  #  Returns the line number where an event occurred,
   def lineno
     @event[:lineno]
   end
 
-  #
   # @return [Module]
-  #   Returns the class or module where an event occurred.
-  #
+  #  Returns the class or module where an event occurred.
   def from_module
     @event[:from_module]
   end
 
-  #
   # @return [Symbol]
-  #   Returns name of a method where an event occurred.
-  #
+  #  Returns the method name where an event occurred.
   def from_method
     @event[:from_method]
   end
 
-  #
   # @return [Binding]
-  #   Returns a Binding object in context of where an event occurred.
-  #
+  #  Returns a Binding object in the context of where an event occurred.
   def binding
     @event[:binding]
   end
 
-  #
   # @return [Boolean]
-  #   Returns true when an event is a method call by a method implemented in C.
-  #
+  #  Returns true when an event is for a C method call.
+  def c_return?
+    @type == "c-return"
+  end
+
+  # @return [Boolean]
+  #  Returns true when an event is for a Ruby method return.
+  def rb_return?
+    @type == "return"
+  end
+
+  # @return [Boolean]
+  #   Returns true when an event is for a C method call.
   def c_call?
     @type == "c-call"
   end
 
-  #
   # @return [Boolean]
-  #   Returns true when an event is a method call by a method implemented in Ruby.
-  #
+  #   Returns true when an event is for a Ruby method call.
   def rb_call?
     @type == "call"
   end
 
   # @return [Boolean]
-  #  Returns true if the event is a Ruby or C method call.
+  #  Returns true if the event is for a Ruby or C method call.
   def call?
     c_call? || rb_call?
   end
 
   # @return [Boolean]
-  #  Returns true if the event is a Ruby or C method return.
+  #  Returns true if the event is for a Ruby or C method return.
   def return?
     c_return? || rb_return?
   end
 
-  #
-  # @return [Boolean]
-  #   Returns true when an event is a method return by a method implemented in C.
-  #
-  def c_return?
-    @type == "c-return"
-  end
-
-  #
-  # @return [Boolean]
-  #   Returns true when an event is a method return by a method implemented in Ruby.
-  #
-  def rb_return?
-    @type == "return"
-  end
-
-  #
   # @return [String]
-  #
   def inspect
     "#<Trip::Event:0x#{__id__.to_s(16)} " \
     "type='#{type}'" \
@@ -111,10 +86,8 @@ class Trip::Event < BasicObject
     "binding=#{binding.inspect}>"
   end
 
-  #
   # @return [Binding]
-  #   Returns a binding object in the context of an instance of {Trip::Event}.
-  #
+  #  Returns a binding object for an instance of {Trip::Event}.
   def __binding__
     ::Kernel.binding
   end
