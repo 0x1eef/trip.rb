9ae52bd9402f4ff45ed7959437b5eb4e6b8d0a69
0x1eef <0x1eef@protonmail.com>
2022-04-14 07:22:01 -0300

    Trip::Event: improve docs

--- lib/trip/event.rb
+++ lib/trip/event.rb
@@ -1,35 +1,39 @@
 # frozen_string_literal: true
 
+##
+# {Trip::Event} represents one of the following
+# TracePoint events:
+#
+#  * `:c_call` <br>
+#     When a method implemented in C is called. <br><br>
+#  * `:c_return` <br>
+#     When a method implemented in C returns. <br><br>
+#  * `:call` <br>
+#     When a method implemented in Ruby is called. <br><br>
+#  * `:return` <br>
+#     When a method implemented in Ruby returns. <br><br>
+#  * `:class` <br>
+#     When a module / class is defined or reopened using the "module"
+#     or "class" keywords. <br><br>
+#  * `:end` <br>
+#     When a module / class definition or reopening ends. <br><br>
+#  * `:line` <br>
+#     When on a line that starts an expression or statement. <br><br>
+#  * `:raise` <br>
+#     When an exception is raised. <br><br>
+#  * `:b_call` <br>
+#     When a block is called. <br><br>
+#  * `:b_return` <br>
+#     When a block returns. <br><br>
+#  * `:thread_begin` <br>
+#     When a thread begins.<br><br>
+#  * `:thread_end` <br>
+#     When a thread ends.<br><br>
+#  * `:fiber_switch` <br>
+#     When a Fiber switches context. <br><br>
+#  * `:script_compiled` <br>
+#     When Ruby code is compiled by `eval`, `require`, or `load`.
 class Trip::Event < BasicObject
-  ##
-  # Returns one of the following event names:
-  #
-  #  * <code>:c_call</code>
-  #  * <code>:c_return</code>
-  #  * <code>:call</code>
-  #  * <code>:return</code>
-  #  * <code>:class</code>
-  #  * <code>:end</code>
-  #  * <code>:line</code>
-  #  * <code>:raise</code>
-  #  * <code>:b_call</code>
-  #  * <code>:b_return</code>
-  #  * <code>:thread_begin</code>
-  #  * <code>:thread_end</code>
-  #  * <code>:fiber_switch</code>
-  #  * <code>:script_compiled</code>
-  #
-  # @see https://docs.w3cub.com/ruby~3/tracepoint#class-TracePoint-label-Events
-  #
-  # @return [Symbol]
-  #  An event name.
-  attr_reader :name
-
-  ##
-  # @return [Integer]
-  #  Number of seconds since epoch.
-  attr_reader :created_at
-
   def initialize(name, tp_details)
     @name = name
     @tp_details = tp_details
@@ -37,31 +41,43 @@ class Trip::Event < BasicObject
   end
 
   ##
+  # @return [Symbol]
+  #  An event name.
+  def name
+    @name
+  end
+
+  ##
+  # @return [Integer]
+  #  Number of seconds since epoch.
+  def created_at
+    @created_at
+  end
+
+  ##
   # @return [String]
-  #  Returns the path where an event occurred.
+  #  Returns the path associated with an event.
   def path
     @tp_details[:path]
   end
 
   ##
   # @return [Integer]
-  #  Returns the line number where an event occurred.
+  #  Returns the line number associated with an event.
   def lineno
     @tp_details[:lineno]
   end
 
   ##
   # @return [Object, BasicObject]
-  #  Returns the value of self in the context of where
-  #  an event occurred.
+  #  Returns the `self` where an event occurred.
   def self
     @tp_details[:self]
   end
 
   ##
   # @return [Module]
-  #  Returns the Module in the context of where an
-  #  event occurred.
+  #  Returns the module or class associated with an event.
   def module
     event_self = self.self
     ::Module === event_self ? event_self : event_self.class
@@ -69,57 +85,50 @@ class Trip::Event < BasicObject
 
   ##
   # @return [Symbol]
-  #  Returns the ID (name) of the method where an event
-  #  occurred.
+  #  Returns the method id associated with an event.
   def method_id
     @tp_details[:method_id]
   end
 
   ##
   # @return [Boolean]
-  #  Returns true when the event is for the definition of
-  #  a class or module using "class Name" or "module Name"
-  #  syntax.
+  #  Returns true when an event is for the definition or
+  #  reopening of a module / class.
   def module_def?
     name == :class
   end
 
   ##
   # @return [Binding]
-  #  Returns a Binding object in the context of where
-  #  an event occurred.
+  #  Returns a Binding object bound to where an event occurred.
   def binding
     @tp_details[:binding]
   end
 
   ##
   # @return [Boolean]
-  #  Returns true when an event is a call from a
-  #  method implemented in Ruby.
+  #  Returns true for a call to a method implemented in Ruby.
   def rb_call?
     @name == :call
   end
 
   ##
   # @return [Boolean]
-  #  Returns true when an event is a return from a
-  #  method implemented in Ruby.
+  #  Returns true for a return from a method implemented in Ruby.
   def rb_return?
     @name == :return
   end
 
   ##
   # @return [Boolean]
-  #  Returns true when an event is a call to a method
-  #  implemented in C.
+  #  Returns true for a call to a method implemented in C.
   def c_call?
     @name == :c_call
   end
 
   ##
   # @return [Boolean]
-  #  Returns true when an event is a return from a
-  #  method implemented in C.
+  #  Returns true for a return from a method implemented in C.
   def c_return?
     @name == :c_return
   end
