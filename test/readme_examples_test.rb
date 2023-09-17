6f62b645e36ad4bdac12bcfde09c18670ac9bb37
0x1eef <0x1eef@protonmail.com>
2023-09-17 05:10:42 -0300

    Add share/trip.rb/examples/

--- test/readme_examples_test.rb
+++ test/readme_examples_test.rb
@@ -5,16 +5,22 @@ class Trip::ReadmeExamplesTest < Test::Unit::TestCase
 
   def test_1_what_is_a_concurrent_tracer
     assert_equal "Ruby is cool.\n",
-                 cmd("readme_examples/1_what_is_a_concurrent_tracer.rb").stdout
+                 cmd(example("1_what_is_a_concurrent_tracer.rb")).stdout
   end
 
   def test_2_filter_events
     assert_equal "call add\n70\nreturn add\n",
-                 cmd("readme_examples/2_filter_events.rb").stdout
+                 cmd(example("2_filter_events.rb")).stdout
   end
 
   def test_3_pause_when
     assert_equal "Foo class opened\nBar class opened\nBaz class opened\n",
-                 cmd("readme_examples/3_trip_pause_when.rb").stdout
+                 cmd(example("3_trip_pause_when.rb")).stdout
+  end
+
+  private
+
+  def example(filename)
+    File.join(Dir.getwd, "share", "trip.rb", "examples", filename)
   end
 end
