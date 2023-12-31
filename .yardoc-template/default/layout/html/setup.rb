f36964b8c5359041c7b2cbe041a8baec2c23e43d
0x1eef <0x1eef@protonmail.com>
2023-12-31 20:40:48 -0300

    rm .yardoc-template/

--- .yardoc-template/default/layout/html/setup.rb
+++
@@ -1,5 +0,0 @@
-def stylesheets
-  s = super
-  s << "css/0x1eef.css"
-  s
-end
--- .yardoc-template/default/module/setup.rb
+++
@@ -1,7 +0,0 @@
-##
-# Sort methods in ascending order based
-# on the line number where a method is
-# defined.
-def sort_listing(listing)
-  listing.sort_by { _1.files[1] }
-end
